9499d1a78c01e9bcc36408a14dfc62ece8d627fe
jasonodoom <jasonodoom@riseup.net>
2018-10-22 16:27:41 -0400

    made db changes and removed db and schema from .gitignore.

--- db/migrate/20181022195040_create_players.rb
+++ db/migrate/20181022195040_create_players.rb
@@ -2,6 +2,9 @@ class CreatePlayers < ActiveRecord::Migration[5.0]
   def change
     create_table :players do |t|
       t.string :name
+      t.integer :age
+      t.string :occupation, default: "Detective"
+      t.string :description, default: "Just a stranger."
     end
   end
 end
--- db/migrate/20181022195420_create_characters.rb
+++ db/migrate/20181022195420_create_characters.rb
@@ -2,7 +2,8 @@ class CreateCharacters < ActiveRecord::Migration[5.0]
   def change
     create_table :characters do |t|
       t.string :name
-      t.boolean :guilty
+      t.string :decription
+      t.boolean :guilty, default: false
     end
   end
 end
