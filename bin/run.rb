4924fa7d764bac06aee9853ef0c645f8b5968875
jasonodoom <jasonodoom@riseup.net>
2018-10-23 10:09:34 -0400

    added starter methods

--- bin/run.rb
+++ bin/run.rb
@@ -1,11 +1,33 @@
 require_relative '../config/environment'
 
+def loader
+  art = <<-EOS
+___________       __                   __  .__             _________        .__
+\_   _____/ _____/  |_  ___________  _/  |_|  |__   ____   \_   ___ \_______|__| _____   ____
+ |    __)_ /    \   __\/ __ \_  __ \ \   __\  |  \_/ __ \  /    \  \/\_  __ \  |/     \_/ __ \
+ |        \   |  \  | \  ___/|  | \/  |  | |   Y  \  ___/  \     \____|  | \/  |  Y Y  \  ___/
+/_______  /___|  /__|  \___  >__|     |__| |___|  /\___  >  \______  /|__|  |__|__|_|  /\___  >
+        \/     \/          \/                   \/     \/          \/                \/     \/
+                        ▲ Enter the Crime ▲
+                     (a game by Randy & Jason)
+EOS
+
+print art
+end
+
 def start_game
+  loader
+  puts " What is your name?"
+  player_name = gets.chomp
 end
 
 def intro
 puts "It's a dark and stormy night. We find ourselves in an old Victorian home,
-somewhere in Brooklyn, New York. Unlike most evenings, this one was different.
+somewhere in Brooklyn, New York.
+
+Unlike most evenings, this one was different.
+
+
 Why? Because on this particular evening, a murder had been commited.
 
 You are a detective, tasked with investigating this incident and finding the
@@ -17,12 +39,12 @@ Do you accept this investigation and all associated risks?[y/n]"
 user_choice = gets.chomp
 
 if user_choice.downcase == "yes"|| user_choice.downcase == "y"
-"Then continue forward... "
+ puts "Then continue forward... "
 start_game
 elsif user_choice.downcase == "no" || user_choice.downcase == "n"
-"Then turn away..."
+ puts "Then turn away..."
+end
 end
 
-end
 
 intro
