7bb6fa837aea78fe6a0301ee222af825dbc2c1cc
jasonodoom <jasonodoom@riseup.net>
2018-10-24 09:20:12 -0400

    fixed a bit of formatring. working on method to display player name in dialogue.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,7 @@
 class Player < ActiveRecord::Base
   has_many :interrogations
+
+
+  def identify_player
+    #this method should identify the player for use in suspect.rb {Player.all.name}: dialogue here   
 end
